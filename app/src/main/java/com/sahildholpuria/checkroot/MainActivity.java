f1fc0c4c32ff848eac1f5d9ba6e235ed5a4581b7
sahildholpuria <dholpuria1999@gmail.com>
2021-12-26 22:04:52 +0530

    some minor changes

--- app/src/main/java/com/sahildholpuria/checkroot/MainActivity.java
+++ app/src/main/java/com/sahildholpuria/checkroot/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.scottyab.rootbeer.RootBeer;
+
 public class MainActivity extends AppCompatActivity {
     TextView textView;
 
@@ -14,11 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
 
         textView = findViewById(R.id.check_textview);
     }
 
     public void check_btn(View view) {
-        Toast.makeText(this, "Your Phone is Not Rooted", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, "Your Phone is Not Rooted", Toast.LENGTH_SHORT).show();
+        RootBeer rootBeer = new RootBeer(this);
+        if (rootBeer.isRooted()) {
+            //we found indication of root
+            Toast.makeText(this, "Your Phone is Rooted", Toast.LENGTH_SHORT).show();
+            textView.setText("Your Device is Rooted!");
+        } else {
+            //we didn't find indication of root
+            Toast.makeText(this, "Your Phone is Not Rooted", Toast.LENGTH_SHORT).show();
+            textView.setText("Your Device is Not Rooted!");
+        }
     }
 }
