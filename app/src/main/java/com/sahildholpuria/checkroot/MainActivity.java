65d841d2d86e01c99f45c7824fd976b0a6fd4d72
sahildholpuria <dholpuria1999@gmail.com>
2021-12-26 21:50:45 +0530

    bug fix

--- app/src/main/java/com/sahildholpuria/checkroot/MainActivity.java
+++ app/src/main/java/com/sahildholpuria/checkroot/MainActivity.java
@@ -2,16 +2,20 @@ package com.sahildholpuria.checkroot;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.check_textview);
     }
 
     public void check_btn(View view) {
